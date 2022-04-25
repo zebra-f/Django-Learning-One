97818d996308815f0341d50d7d095d7504f27c02
zebra-f <rafal.zebrowski@gmail.com>
2022-04-25 12:44:32 +0200

    dynamic urls

--- first_app/urls.py
+++ first_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.simple_view)
-]
+    path('', views.simple_view),
+    path('sports/', views.sports_view),
+    path('finance/', views.finance_view),
+    path('<topic>/', views.articles_view),
+    
+]       
--- first_app/views.py
+++ first_app/views.py
@@ -4,6 +4,40 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+articles = {
+    "programming": "Programming page",
+    "cars": "Cars page"
+}
+
+
 def simple_view(request):
+    """
+    host/first_app/
+    """
     return HttpResponse("Simple View")
+
+
+def sports_view(request):
+    """
+    host/first_app/sports/
+    """
+    return HttpResponse("Sports Page")
+
+
+def finance_view(reqeust):
+    """
+    host/first_app/finance/
+    """
+    return HttpResponse("Finance Page")
+
+
+def articles_view(request, topic):
+    """
+    host/first_app/<topic>/
+    """
+    return HttpResponse(articles[topic])
+
+
+
+
 
