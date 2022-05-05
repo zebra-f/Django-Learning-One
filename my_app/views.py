f2420bfdda758888a31590695844148b7ecf6471
zebra-f <rafal.zebrowski@gmail.com>
2022-05-05 11:05:35 +0200

    template for loops

--- my_app/views.py
+++ my_app/views.py
@@ -11,7 +11,13 @@ def variables_view(request):
     variables = {
         'first_name': 'John',
         'last_name': 'Williams',
-        'playlist': ['Hedwig\'s Theme', 'Journey to the Island']
+        'playlist': ['Hedwig\'s Theme', 'Journey to the Island', 'Out to Sea', 'Binary Sunset'],
+        'movies_playlist': {
+            'Harry Potter': ['Hedwig\'s Theme', 'Harry\'s Wondrous World', 'Christmas at Hogwarts'],
+            'Jurassic Park': ['Journey to the Island'],
+            'Jaws': ['Out to Sea'],
+            'Star Wars': ['Binary Sunset', 'Princess Leia\'s Theme']
+        }
     }
 
     return render(request, 'my_app/variables.html', context=variables)
